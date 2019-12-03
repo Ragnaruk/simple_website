14e2caaccf5dbd33971f6f17f95fe26cf38e8138
Ragnaruk <ima1365@me.com>
2019-12-03 15:00:13 +0300

    Added graph

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 https://coder-coder.com/build-flexbox-website-layout/
 """
 from flask import Flask, render_template, url_for
+from pathlib import Path
 
 app = Flask(__name__)
 
@@ -13,7 +14,30 @@ def landing():
 
 @app.route('/graph')
 def graph():
-    return render_template('graph.html')
+    with open(Path(__file__).parent / "data" / "price.csv", "r") as f:
+        price = f.read()
+
+    price_labels = []
+    price_data = []
+    for pr in price.split('\n'):
+        try:
+            d = pr.split(',')
+
+            price_labels.append(
+                d[0]
+            )
+
+            price_data.append(
+                d[4]
+            )
+        except Exception:
+            pass
+
+    return render_template(
+        'graph.html',
+        price_labels=price_labels,
+        price_data=price_data
+    )
 
 
 @app.route('/news')
